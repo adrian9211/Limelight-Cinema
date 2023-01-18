8c2fac5521bda059c628ad2ef405644994060ebd
Adrian Nykiel <adrian.nykiel@gmail.com>
2023-01-18 14:19:15 +0000

    contact page map and icons

--- about.php
+++ about.php
@@ -98,35 +98,7 @@ include('includes/header.php');
     </div>
 </div>
 <!--Feedback from our customers owl-carousel-->
-<!-- pop-up-box -->
-<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-<!--//pop-up-box -->
-<div id="small-dialog" class="mfp-hide">
-    <iframe src="https://player.vimeo.com/video/164819130?title=0&byline=0"></iframe>
-</div>
-<div id="small-dialog1" class="mfp-hide">
-    <iframe src="https://player.vimeo.com/video/148284736"></iframe>
-</div>
-<div id="small-dialog2" class="mfp-hide">
-    <iframe src="https://player.vimeo.com/video/165197924?color=ffffff&title=0&byline=0&portrait=0"></iframe>
-</div>
-<script>
-    $(document).ready(function() {
-        $('.Limelight_play_icon,.Limelight_play_icon1,.Limelight_play_icon2').magnificPopup({
-            type: 'inline',
-            fixedContentPos: false,
-            fixedBgPos: true,
-            overflowY: 'auto',
-            closeBtnInside: true,
-            preloader: false,
-            midClick: true,
-            removalDelay: 300,
-            mainClass: 'my-mfp-zoom-in'
-        });
 
-    });
-</script>
-<!-- //Latest-tv-series -->
 
 <?php
 # Include footer
--- contact.php
+++ contact.php
@@ -6,41 +6,74 @@ $page_title = "Limelight Cinema - Contact";
 include('includes/header.php');
 //$_SESSION['loggedin'] = 0;
 ?>
+<iframe
+        width="100%"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120519.31565742021!2d-3.2476778117608336!3d55.85550172864269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4887b9a7365fccff%3A0x52629bc613f4b94b!2sEdinburgh%20College!5e0!3m2!1spl!2suk!4v1674021147748!5m2!1spl!2suk">
+</iframe>
 
-<div class="container text-center about-page">
-    <h1>Limelight Cinema - Contact Page</h1>
-    <h2>Building in progress...</h2>
+<h4 class="latest-text Limelight_latest_text mt-5">Contact Us</h4>
+
+<div class="container text-center contact-page">
+    <div class="row">
+        <div class="col-xl-3 col-sm-6 col-xs-12">
+            <section class="contact-box">
+                <div class="circle-icon mt-3">
+                    <i class="fa fa-phone "></i>
+                </div>
+                <h4 class="contact-text"><b>Phone</b></h4>
+                <p class="contact-text">Customer Service</p>
+                <p class="contact-text">0333 003 3450</p>
+                <p class="contact-text">Sales</p>
+                <p class="contact-text">0333 003 3411</p>
+            </section>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-xs-12">
+            <section class="contact-box">
+                <div class="circle-icon mt-3">
+                    <i class="fa fa-envelope "></i>
+                </div>
+                <h4 class="contact-text"><b>Email</b></h4>
+                <p class="contact-text">customer.services@limelight.co.uk</p>
+                <p class="contact-text">or</p>
+                <p class="contact-text">general.enquires@ limelight.co.uk</p>
+            </section>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-xs-12">
+            <section class="contact-box">
+                <div class="circle-icon mt-3">
+                    <i class="fa fa-map"></i>
+                </div>
+                <h4 class="contact-text"><b>Address</b></h4>
+                <p class="contact-text">Edinburgh College</p>
+                <p class="contact-text">Milton Rd, Campus</p>
+                <p class="contact-text">Edinburgh</p>
+                <p class="contact-text">EH11 4BN</p>
+            </section>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-xs-12">
+            <section class="contact-box">
+                <div class="circle-icon mt-3">
+                    <i class="fa fa-user "></i>
+                </div>
+                <h4 class="contact-text"><b>Social media</b></h4>
+                <p class="contact-text">Edinburgh College</p>
+                <p class="contact-text">Milton Rd, Campus</p>
+                <p class="contact-text">Edinburgh</p>
+                <p class="contact-text">EH11 4BN</p>
+            </section>
+        </div>
+    </div>
 </div>
 
-<!-- pop-up-box -->
-<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-<!--//pop-up-box -->
-<div id="small-dialog" class="mfp-hide">
-    <iframe src="https://player.vimeo.com/video/164819130?title=0&byline=0"></iframe>
-</div>
-<div id="small-dialog1" class="mfp-hide">
-    <iframe src="https://player.vimeo.com/video/148284736"></iframe>
-</div>
-<div id="small-dialog2" class="mfp-hide">
-    <iframe src="https://player.vimeo.com/video/165197924?color=ffffff&title=0&byline=0&portrait=0"></iframe>
-</div>
-<script>
-    $(document).ready(function() {
-        $('.Limelight_play_icon,.Limelight_play_icon1,.Limelight_play_icon2').magnificPopup({
-            type: 'inline',
-            fixedContentPos: false,
-            fixedBgPos: true,
-            overflowY: 'auto',
-            closeBtnInside: true,
-            preloader: false,
-            midClick: true,
-            removalDelay: 300,
-            mainClass: 'my-mfp-zoom-in'
-        });
 
-    });
-</script>
-<!-- //Latest-tv-series -->
 
 <?php
 # Include footer
